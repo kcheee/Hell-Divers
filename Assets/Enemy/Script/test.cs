b46d0351d1b990feacb3564f37e9c076bbacc785
kcheee <cksgml1101@naver.com>
2023-08-12 03:50:43 +0900

    push
    push

--- Assets/Enemy/Script/test.cs
+++ Assets/Enemy/Script/test.cs
@@ -1,26 +1,40 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    float FallingTime;
-    float m_fAngle = 30;
+    public Transform player; // 플레이어 오브젝트
+    public float rotationAngle = 40f; // 회전할 각도 (40도)
+    public float rotationDuration = 2f; // 회전할 시간 (2초)
 
-    public GameObject grenadePrefab; // 유탄 프리팹
-
-
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        //FallingTime += Time.fixedDeltaTime;
 
-        if (Input.GetKeyDown(KeyCode.C))
+        // 자신의 기준 forward 방향 벡터
+        Vector3 forwardDirection = transform.forward;
+
+        // 오른쪽으로 회전
+        RotateRight(forwardDirection);
+    }
+    private void OnDisable()
+    {
+        // 각도 0으로 만듦.
+        transform.rotation = Quaternion.Euler(0,0,0);
+    }
+
+    // 자신을 기준으로 오른쪽으로 회전
+    private void RotateRight(Vector3 forwardDirection)
+    {
+        // 1초동안 오른쪽으로 갔다가 왼쪽으로 감
+        Quaternion targetRotation = Quaternion.Euler(0, rotationAngle, 0) * Quaternion.LookRotation(forwardDirection);
+        transform.DORotateQuaternion(targetRotation, 1f).OnComplete(() =>
         {
-           
-        }
-        //Falling(5);
-        //Moving(5);
+            targetRotation = Quaternion.Euler(0, -rotationAngle, 0) * Quaternion.LookRotation(forwardDirection);
+            transform.DORotateQuaternion(targetRotation, 1f);
+        });
     }
 
 }
